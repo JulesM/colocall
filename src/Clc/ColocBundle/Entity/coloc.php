5dc0932c2d9d5db1dfe129cbb17171504dacdf75
JulesM <jules.marcilhacy@gmail.com>
2013-02-27 16:00:07 +0100

    Création de l'entité 'coloc'

---
+++ src/Clc/ColocBundle/Entity/coloc.php
@@ -0,0 +1,304 @@
+<?php
+
+namespace Clc\ColocBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * coloc
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Clc\ColocBundle\Entity\colocRepository")
+ */
+class coloc
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address1", type="string", length=255)
+     */
+    private $address1;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address2", type="string", length=255)
+     */
+    private $address2;
+
+    /**
+     * @var smallint
+     *
+     * @ORM\Column(name="zipcode", type="smallint")
+     */
+    private $zipcode;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="country", type="string", length=255)
+     */
+    private $country;
+
+    /**
+     * @var smallint
+     *
+     * @ORM\Column(name="number", type="smallint")
+     */
+    private $number;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="other", type="text")
+     */
+    private $other;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return coloc
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return coloc
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set address1
+     *
+     * @param string $address1
+     * @return coloc
+     */
+    public function setAddress1($address1)
+    {
+        $this->address1 = $address1;
+    
+        return $this;
+    }
+
+    /**
+     * Get address1
+     *
+     * @return string 
+     */
+    public function getAddress1()
+    {
+        return $this->address1;
+    }
+
+    /**
+     * Set address2
+     *
+     * @param string $address2
+     * @return coloc
+     */
+    public function setAddress2($address2)
+    {
+        $this->address2 = $address2;
+    
+        return $this;
+    }
+
+    /**
+     * Get address2
+     *
+     * @return string 
+     */
+    public function getAddress2()
+    {
+        return $this->address2;
+    }
+
+    /**
+     * Set zipcode
+     *
+     * @param integer $zipcode
+     * @return coloc
+     */
+    public function setZipcode($zipcode)
+    {
+        $this->zipcode = $zipcode;
+    
+        return $this;
+    }
+
+    /**
+     * Get zipcode
+     *
+     * @return integer 
+     */
+    public function getZipcode()
+    {
+        return $this->zipcode;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     * @return coloc
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string 
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     * @return coloc
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string 
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Set number
+     *
+     * @param integer $number
+     * @return coloc
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+    
+        return $this;
+    }
+
+    /**
+     * Get number
+     *
+     * @return integer 
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * Set other
+     *
+     * @param string $other
+     * @return coloc
+     */
+    public function setOther($other)
+    {
+        $this->other = $other;
+    
+        return $this;
+    }
+
+    /**
+     * Get other
+     *
+     * @return string 
+     */
+    public function getOther()
+    {
+        return $this->other;
+    }
+}
