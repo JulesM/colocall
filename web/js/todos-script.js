e8d5222e643adae8089bfdcd0dbebc9bf318c9aa
ArnaudDri <arnaud.drizard@gmail.com>
2013-03-21 23:27:03 +0100

    todos

---
+++ web/js/todos-script.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function(){
+    $( "#todo-list tbody" ).sortable();
+    $( "#todo-list tbody" ).disableSelection();
+    
+        $('#todo-list tbody tr').mouseenter(function() {
+        $(this).css('background-color', 'rgba(255, 255, 255, 0.2)');
+        
+    });
+    
+        $('#todo-list tbody tr').mouseleave(function() {
+        $(this).css('background-color', 'transparent');
+    });
+    
+    $('#todo-check').click(function() {
+        $('#todo-list tbody tr').fadeOut('slow');
+    });
+
+});
