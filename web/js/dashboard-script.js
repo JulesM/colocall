88ec7cfd44560a9420cb5b584308fb537faac38d
ArnaudDri <arnaud.drizard@gmail.com>
2013-03-25 18:08:00 +0100

    Javascript trop beau sur le nouvreau dashboard encore plus beau

---
+++ web/js/dashboard-script.js
@@ -0,0 +1,24 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+//$(document).ready(function(){
+//    $( "#todo-list tbody" ).sortable();
+//    $( "#todo-list tbody" ).disableSelection();
+//    
+//        $('#todo-list tbody tr').accordion();
+//    
+//        $('#todo-list tbody tr').mouseleave(function() {
+//        $(this).css('background-color', 'transparent');
+//    });
+//    
+//});
+
+
+$(document).ready(function(){
+ $(function() {
+$( "#accordion" ).accordion();
+});
+});
