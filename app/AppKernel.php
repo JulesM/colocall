126635f5e78989fec9c3539fb402525655852c25
JulesM <jules.marcilhacy@gmail.com>
2013-04-03 16:56:31 +0200

    Error commit

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,7 +27,6 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Clc\UserBundle\ClcUserBundle(),
             new Clc\EventBundle\ClcEventBundle(),
-            new Clc\InboxBundle\ClcInboxBundle(),
             new Clc\ColocBundle\ClcColocBundle(),
         );
 
