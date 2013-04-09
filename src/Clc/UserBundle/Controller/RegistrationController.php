2f8e08c8ce2d0de57b81bae1b045ef5a4f01515d
JulesM <jules.marcilhacy@gmail.com>
2013-04-09 20:33:30 +0200

    Redirection après register

--- src/Clc/UserBundle/Controller/RegistrationController.php
+++ src/Clc/UserBundle/Controller/RegistrationController.php
@@ -3,11 +3,28 @@
 
 namespace Clc\UserBundle\Controller;
 
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use FOS\UserBundle\Controller\RegistrationController as BaseController;
 use FOS\UserBundle\Doctrine\UserManager;
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Exception\AccountStatusException;
+use FOS\UserBundle\Model\UserInterface;
 
 class RegistrationController extends BaseController
 {
-    
+    /**
+     * Tell the user his account is now confirmed
+     */
+    public function confirmedAction()
+    {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        return $this->container->get('templating')->renderResponse('ClcDashboardBundle:JoinColoc:join.html.twig');
+    }
 }
