e0adfc6cf806babe78149b6f688f08fc422ef176
JulesM <jules.marcilhacy@gmail.com>
2013-04-16 17:41:05 +0200

    Feedback form

--- src/Clc/UserBundle/Controller/UserController.php
+++ src/Clc/UserBundle/Controller/UserController.php
@@ -4,11 +4,50 @@
 namespace Clc\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Clc\UserBundle\Entity\feedback;
 
 class UserController extends Controller
 {
     public function welcomeAction()
     {
         return $this->render('ClcUserBundle::welcome.html.twig');
+    }
+    
+    public function feedbackAction()
+    {
+        $feedback = new feedback();
+        $feedback->setDate( new \DateTime('now'))
+                 ->setAuthor($this->getUser());
+        
+        $form = $this->createFormBuilder($feedback)
+                     ->add('category', 'choice', array(
+                            'choices' => array(
+                                'suggestion' => 'Suggestion',
+                                'question'   => 'Question',
+                                'bug'        => 'Bug'
+                                
+                            )))
+                     ->add('text', 'textarea')
+                     ->getForm();
+        
+        $request = $this->get('request');
+
+        if ($request->isMethod('POST')) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                    
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($feedback);
+                $em->flush();
+                
+                $url = $this->getRequest()->headers->get("referer");
+                return $this->redirect($url);
+            }
+        }
+                
+        return $this->render('ClcUserBundle:Feedback:feedbackForm.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
