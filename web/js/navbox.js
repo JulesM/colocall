fd72dcba76f015d98ae1d7d12f445372bc9ccdc6
JulesM <jules.marcilhacy@gmail.com>
2013-04-25 12:11:42 +0200

    Pointeur removed in dashboard

--- web/js/navbox.js
+++ web/js/navbox.js
@@ -10,23 +10,23 @@ $(function(){
     var page  = document.URL;
     if (page.search("inbox")>0)
     {
-    $('#navbox-link-1').addClass('test');
+        $('#navbox-link-1').addClass('test');
     }else if (page.search("expense")>0)
     {
-    $('#navbox-link-2').addClass('test');
+        $('#navbox-link-2').addClass('test');
     }else if (page.search("todos")>0)    
     {
-    $('#navbox-link-3').addClass('test');
+        $('#navbox-link-3').addClass('test');
     }else if (page.search("shoppinglist")>0)    
     {
-    $('#navbox-link-4').addClass('test');
-    }else if (page.search("coloc")>0 && page.search("coloc/statistics")<0)    
+        $('#navbox-link-4').addClass('test');
+    }else if (page.search("coloc/")>0 && page.search("coloc/statistics")<0)    
     {
-    $('#navbox-link-5').addClass('test');
+        $('#navbox-link-5').addClass('test');
     }else if (page.search("coloc/statistics")>0)    
     {
-    $('#navbox-link-6').addClass('test');
-    }     
+        $('#navbox-link-6').addClass('test');
+    }       
 });
 
 
